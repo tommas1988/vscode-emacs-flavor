9ce8a10dec76ec4eb280564c5dd061186722b141
v_yuanlin <lin-yuan@neusoft.com>
2018-05-21 15:47:30 +0800

    fixed compile errors

--- src/Buffer.ts
+++ src/Buffer.ts
@@ -9,13 +9,13 @@ export class Buffer {
     private buffers: WeakMap<vscode.TextEditor, BufferEntry> = new WeakMap();
 
     public getActiveBuffer(): BufferEntry {
-        let editor = vscode.window.activeTextEditor;
+        let editor = <vscode.TextEditor> vscode.window.activeTextEditor;
         if (!this.buffers.has(editor)) {
             this.buffers.set(editor, {
                 markRing: new MarkRing(),
             }); 
         }
 
-        return this.buffers.get(editor);
+        return <BufferEntry> this.buffers.get(editor);
     }
 }
